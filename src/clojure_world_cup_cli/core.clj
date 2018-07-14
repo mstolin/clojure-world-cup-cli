6f408d2ae68e2acaefaf6eddeb1c2fb3089084f6
Marcel Stolin <marcelstolin@gmail.com>
2018-07-15 00:48:48 +0200

    move the group stuff to group-handler

--- src/clojure_world_cup_cli/core.clj
+++ src/clojure_world_cup_cli/core.clj
@@ -2,7 +2,6 @@
   (:require [clojure-world-cup-cli.handler.group-handler :as group-handler]
             [clojure-world-cup-cli.handler.stadium-handler :as stadium-handler]
             [clojure-world-cup-cli.handler.team-handler :as team-handler]
-            [clojure.pprint :as pprint :refer [pprint print-table]]
             [clojure.tools.cli :refer [parse-opts]]
             [clj-http.client :as client]
             [cheshire.core :refer :all])
@@ -48,40 +47,13 @@
        (format "  %-10s %s" "group" "Shows a specific group (a - h)")
        (format "  %-10s %s" "team" "Shows a specific team")
        ""])))
-
-(defn print-group [group teams stadiums]
-  (let [{:keys [name winner runnerup matches]} group]
-    (do
-      (println
-        (clojure.string/join "\n" 
-          [(clojure.string/upper-case name)
-          ""
-          (format "%s %-10s %s" "🥇" "Winner:" (get (team-handler/get-first-team teams winner) :emojiString))
-          (format "%s %-10s %s" "🥈" "Runner-Up:" (get (team-handler/get-first-team teams runnerup) :emojiString))
-          ""
-          ""]))
-      (println "MATCHES:")
-      (print-table ["Date" "Result" "Stadium"]
-        (reverse 
-          (for [keyVal matches] 
-            (hash-map 
-              "Date" (.format (java.text.SimpleDateFormat. "MMM d yyyy, h:mm a") (.parse (java.text.SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ssXXX") (get keyVal :date)))
-              "Result" (format 
-                        "%s %s:%s %s"
-                        (get (team-handler/get-first-team teams (get keyVal :home_team)) :emojiString)
-                        (get keyVal :home_result)
-                        (get keyVal :away_result)
-                        (get (team-handler/get-first-team teams (get keyVal :away_team)) :emojiString))
-              "Stadium" (let [stadium (stadium-handler/get-first-stadium stadiums (get keyVal :stadium))]
-                          (format
-                            "%s (%s)"
-                            (get stadium :name)
-                            (get stadium :city))))))))))
     
 (defn show-group [options groups teams stadiums]
   (let [{:keys [name]} options]
     (if-let [group (get groups (keyword name))]
-      (print-group group teams stadiums)
+      (do
+        (group-handler/print-group-winner group teams stadiums)
+        (group-handler/print-group-matches group teams stadiums))
       (println "No such group"))))
 
 (defn show-team [options teams]
--- src/clojure_world_cup_cli/handler/group_handler.clj
+++ src/clojure_world_cup_cli/handler/group_handler.clj
@@ -1,5 +1,41 @@
 (ns clojure-world-cup-cli.handler.group-handler
+    (:require [clojure-world-cup-cli.handler.stadium-handler :as stadium-handler]
+              [clojure-world-cup-cli.handler.team-handler :as team-handler]
+              [clojure.pprint :as pprint :refer [pprint print-table]])
     (:gen-class))
 
-(defn print-bla []
-    (println "BLAAAA"))
+(defn print-group-winner [group teams stadiums]
+  (let [{:keys [name winner runnerup matches]} group]
+    (println
+        (clojure.string/join "\n" 
+        [(clojure.string/upper-case name)
+        ""
+        (format "%s %-10s %s" "🥇" "Winner:" (get (team-handler/get-first-team teams winner) :emojiString))
+        (format "%s %-10s %s" "🥈" "Runner-Up:" (get (team-handler/get-first-team teams runnerup) :emojiString))
+        ""
+        ""]))))
+
+(defn print-group-matches [group teams stadiums]
+    (let [{:keys [matches]} group]
+        (do 
+            (println "MATCHES:")
+            (print-table ["Date" "Result" "Stadium"]
+                (reverse 
+                (for [keyVal matches] 
+                    (hash-map 
+                    "Date" (.format (java.text.SimpleDateFormat. "MMM d yyyy, h:mm a") (.parse (java.text.SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ssXXX") (get keyVal :date)))
+                    "Result" (if-let [home-team (team-handler/get-first-team teams (get keyVal :home_team))]
+                                    (if-let [away-team (team-handler/get-first-team teams (get keyVal :away_team))]
+                                        (format 
+                                        "%-8s %s:%s %-8s"
+                                        (get home-team :name)
+                                        (get keyVal :home_result)
+                                        (get keyVal :away_result)
+                                        (get away-team :name)))
+                                    "No results for this match given")
+                    "Stadium" (if-let [stadium (stadium-handler/get-first-stadium stadiums (get keyVal :stadium))]
+                                (format
+                                    "%s (%s)"
+                                    (get stadium :name)
+                                    (get stadium :city))
+                                "No such stadium"))))))))
