2129e9c93ba1f9028f8c93d840c58c5c4a7af91f
Marcel Stolin <marcelstolin@gmail.com>
2018-08-08 17:38:52 +0200

    add some essentials group functions

--- src/clojure_world_cup_cli/handler/group_handler.clj
+++ src/clojure_world_cup_cli/handler/group_handler.clj
@@ -23,9 +23,40 @@
 
 (defn get-games [matches team-id]
     (filter
-        (some-fn #(= (:home_team %) team-id)
-                 #(= (:away_team %) team-id)) matches))
+        (some-fn
+            #(= (:home_team %) team-id)
+            #(= (:away_team %) team-id)) matches))
+
+(defn get-wins [matches team-id]
+    (filter
+        #(let [home-team (% :home_team) away-team (% :away_team) home-result (% :home_result) away-result (% :away_result)]
+            (or
+                (and (= home-team team-id) (> home-result away-result))
+                (and (= away-team team-id) (> away-result home-result)))) matches))
+
+(defn get-losses [matches team-id]
+    (filter
+        #(let [home-team (% :home_team) away-team (% :away_team) home-result (% :home_result) away-result (% :away_result)]
+            (or
+                (and (= home-team team-id) (< home-result away-result))
+                (and (= away-team team-id) (< away-result home-result)))) matches))
+
+(defn get-draws [matches team-id]
+    (filter
+        #(let [home-team (% :home_team) away-team (% :away_team) home-result (% :home_result) away-result (% :away_result)]
+            (or
+                (and (= home-team team-id) (= home-result away-result))
+                (and (= away-team team-id) (= away-result home-result)))) matches))
 
 (defn print-stats [matches]
-    (print-table ["Nr." "Name" "Games" "Wins" "Draw" "Losses" "Goals" "Points"]
-        [{"Nr." 1 "Name" "Torben" "Games" (count (get-games matches 1)) "Wins" 1 "Draw" 5 "Losses" 45 "Goals" 234 "Points" 234}]))
+    (print (get-draws (get-games matches 6) 6)))
+    ;(print-table ["Nr." "Name" "Games" "Wins" "Draws" "Losses" "Goals" "Points"]
+    ;    [{
+    ;        "Nr." 1 
+    ;        "Name" "Torben" 
+    ;        "Games" (count (get-games matches 1))
+    ;        "Wins" (count (get-wins (get-games matches 1) 1))
+    ;        "Draw" (count (get-draws (get-games matches 1) 1)) 
+    ;        "Losses" (count (get-losses (get-games matches 1) 1)) 
+    ;        "Goals" 234 
+    ;        "Points" 234}]))
